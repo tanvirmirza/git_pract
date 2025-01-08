e22801af6b221867a7799d7e193da89ff89e083a
tanvirmirza <mirzatanvir74@gmail.com>
2025-01-08 20:15:19 +0600

    add new

---
+++ git-login.dart
@@ -0,0 +1,10 @@
+void main() {
+  String greeting = "Hello";
+  String target = "World";
+
+  printGreeting(greeting, target);
+}
+
+void printGreeting(String greeting, String target) {
+  print('$greeting $target');
+}
